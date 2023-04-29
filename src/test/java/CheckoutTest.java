d6afdaffacc80e4f850b430c566b907817bd1323
ShantalaGaitonde <shantalag4@gmail.com>
2023-04-28 20:53:26 -0400

    Updated the type to include others instead of everything else

--- src/test/java/CheckoutTest.java
+++ src/test/java/CheckoutTest.java
@@ -10,7 +10,7 @@ public class CheckoutTest {
     Checkout checkout = new Checkout();
     List<Item> items = Arrays.asList(
         new Item("Shirt", "Clothing", 20.0),
-        new Item("Book", "everything else", 10.0)
+        new Item("Book", "others", 10.0)
     );
     Double expectedTotal = 30.6;
     Double actualTotal = checkout.calculateTotal("PA", items);
@@ -22,7 +22,7 @@ public class CheckoutTest {
     Checkout checkout = new Checkout();
     List<Item> items = Arrays.asList(
         new Item("Milk", "Wic Eligible food", 5.0),
-        new Item("Cereal", "food", 3.0)
+        new Item("Cereal", "others", 3.0)
     );
     Double expectedTotal = 8.18;
     Double actualTotal = checkout.calculateTotal("PA", items);
@@ -34,7 +34,7 @@ public class CheckoutTest {
     Checkout checkout = new Checkout();
     List<Item> items = Arrays.asList(
         new Item("fur_sweater", "Clothing", 100.0),
-        new Item("Book", "everything else", 10.0)
+        new Item("Book", "others", 10.0)
     );
     Double expectedTotal = 117.26;
     Double actualTotal = checkout.calculateTotal("NJ", items);
@@ -46,7 +46,7 @@ public class CheckoutTest {
     Checkout checkout = new Checkout();
     List<Item> items = Arrays.asList(
         new Item("Milk", "Wic Eligible food", 5.0),
-        new Item("Television", "Everything else", 300.0)
+        new Item("Television", "others", 300.0)
     );
     Double expectedTotal = 324.8;
     Double actualTotal = checkout.calculateTotal("NJ", items);
@@ -58,7 +58,7 @@ public class CheckoutTest {
     Checkout checkout = new Checkout();
     List<Item> items = Arrays.asList(
         new Item("Shirt", "Clothing", 20.0),
-        new Item("Book", "Book", 10.0)
+        new Item("Book", "others", 10.0)
     );
     Double expectedTotal = 30.0;
     Double actualTotal = checkout.calculateTotal("DE", items);
